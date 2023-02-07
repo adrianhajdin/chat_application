ca76de5280ef1e43c570f85304899dd62cfc8615
claudeomosa <claudeomosa@gmail.com>
2023-02-07 21:26:10 +0300

    feat: clear profanity in text sent in messase

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import ChatFeed from './components/ChatFeed';
 import LoginForm from './components/LoginForm';
 import './App.css';
 
-const projectID = '1b7801d6-8a66-4be4-a442-89219d833dfc';
+const projectID = 'e0f03171-59e9-476a-80da-50b0735bfd64';
 
 const App = () => {
   if (!localStorage.getItem('username')) return <LoginForm />;
