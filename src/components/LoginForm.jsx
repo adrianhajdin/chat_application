64b2aaa5f164b7691d41c5a327d543364ccf4e14
Amit Singh <amitsingh@Amits-MacBook-Air.local>
2023-05-23 00:04:08 +0530

    Name Enhancement

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -31,11 +31,11 @@ const Modal = () => {
       <div className="form">
         <h1 className="title">Chat Application</h1>
         <form onSubmit={handleSubmit}>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Username" required />
+          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} className="input" placeholder="Email" required />
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
             <button type="submit" className="button">
-              <span>Start chatting</span>
+              <span>Start exploring</span>
             </button>
           </div>
         </form>
